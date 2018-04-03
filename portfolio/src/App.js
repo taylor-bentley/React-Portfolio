c9fd15f81c2b5e90b2b883b5e20938832b81f355
Taylor Bentley <yogasangerin@gmail.com>
2018-04-03 18:39:52 -0500

    only committing so i can work on something else

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './elements/mediaPage.js';
+import './aboutMe.js';
 
 class App extends Component {
+
+
+
+
   render() {
     return (
-      <div className="Taylor Bentley's Portfolio">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <body>
+          <title className= "Taylor Bentley's Web Portfolio"/>
+          <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css?family=Cabin+Sketch" rel="stylesheet"/>
+
+          <container id = "container"/>
+            <h1>Taylor Bentley</h1>
+            <main>
+              <div id="slider">
+                <img className = "mich" src="img/tayheadshot.jpg" alt="Taylor Bentley's Headshot"/>
+                <article> Welcome to my page! My name is Taylor Bentley, and I am a junior web developer. Take a minute to explore my page, and discover what makes me a unique fit for your company.</article>
+              </div>
+              <section className = "navbar">
+                <div className = "dropdown">Explore
+                  <div className ="dropdown-content">
+                    <a href="aboutMe.js">About Me</a>
+                    <a href="mediaPage.js">Media Page</a>
+                  </div>
+                </div>
+              </section>
+            </main>
+          </body>
     );
   }
 }
