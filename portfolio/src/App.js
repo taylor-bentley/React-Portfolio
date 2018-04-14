6cae6344c48a06972338df9e021ce58814ba3aaa
Taylor Bentley <yogasangerin@gmail.com>
2018-04-14 11:34:56 -0500

    still cant get routes to work

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
-import { Route } from 'react-router-dom'
+
 
 /*child components*/
 import MediaPage from  './elements/MediaPage.js';
@@ -37,9 +36,9 @@ handleOnClick = () => {
               <nav className = "navbar">
                 <div className = "dropdown">Explore
                   <ul className ="dropdown-content">
-                    <li><Link to="file:///Users/taylorbentley/Documents/jsdev/React-Portfolio/portfolio/src/elements/AboutMe.js" target= "_blank" src={AboutMe}>About Me</Link></li> <br/>
-                    <li><Link to="file:///Users/taylorbentley/Documents/jsdev/React-Portfolio/portfolio/src/elements/MediaPage.js"target= "_blank" src={MediaPage}>Media Page</Link></li> <br/>
-                    <li><Link to="file:///Users/taylorbentley/Documents/jsdev/React-Portfolio/portfolio/src/elements/PhotographyPage.js" target= "_blank" src={PhotographyPage}>Photography Page</Link></li>
+                    <li href="file:///Users/taylorbentley/Documents/jsdev/React-Portfolio/portfolio/src/elements/AboutMe.js" target= "_blank" src={AboutMe}>About Me</li> <br/>
+                    <li href="file:///Users/taylorbentley/Documents/jsdev/React-Portfolio/portfolio/src/elements/MediaPage.js"target= "_blank" src={MediaPage}>Media Page</li> <br/>
+                    <li href="file:///Users/taylorbentley/Documents/jsdev/React-Portfolio/portfolio/src/elements/PhotographyPage.js" target= "_blank" src={PhotographyPage}>Photography Page</li>
                   </ul>
                 </div>
               </nav>
